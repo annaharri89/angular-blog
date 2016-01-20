94cf6fdaee2bde67758496965801369e643285e7
Anna Harrison <annaharri89@gmail.com>
2016-01-19 23:18:07 -0500

    Added form validation to newEntryFormController to ensure that an empty blog entry isn't made. If user trys to make empty post they get an alert and the form does not submit.

--- js/newEntryFormController.js
+++ js/newEntryFormController.js
@@ -5,8 +5,16 @@
 angularBlog.controller('NewEntryFormController', ["$scope", "$uibModalInstance", function ($scope, $uibModalInstance) {
     $scope.entry = {};
     $scope.entry.datePub = new Date();
+    $scope.entry.title = "";
+    $scope.entry.body = "";
+    $scope.alert = false;
+
+    $scope
     $scope.submitPost = function () {
-        $uibModalInstance.close($scope.entry);//Sends data (entry) to parent scope.
+        if ($scope.entry.title.length >= 1 && $scope.entry.body.length >= 1) {
+            $uibModalInstance.close($scope.entry);//Sends data (entry) to parent scope.
+        }
+        else { {{$scope.alert = true }} }
     };
 
     $scope.cancel = function () {
