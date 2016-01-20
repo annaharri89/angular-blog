4b7047fc6bc27c64bfde5b3aa54ca6bb83e30171
Anna Harrison <annaharri89@gmail.com>
2016-01-19 21:53:34 -0500

    Added documentation to multiple files.

--- js/blogEntry.js
+++ js/blogEntry.js
@@ -1,6 +1,9 @@
 /**
  * Created by annaharr on 1/18/16.
  */
+/*The blogEntry directive allows html files to create a <blog-entry> element
+which displays the blogEntry html template. It's only used once in the app,
+but it would be very useful in an app with multiple blogs to cut down on code*/
 angularBlog.directive("blogEntry", function() {
     return {
         restrict : "E",
--- js/newEntryFormController.js
+++ js/newEntryFormController.js
@@ -1,11 +1,12 @@
 /**
  * Created by annaharr on 1/19/16.
  */
+//Controller for the newEntryForm modal.
 angularBlog.controller('NewEntryFormController', ["$scope", "$uibModalInstance", function ($scope, $uibModalInstance) {
     $scope.entry = {};
     $scope.entry.datePub = new Date();
     $scope.submitPost = function () {
-        $uibModalInstance.close($scope.entry);
+        $uibModalInstance.close($scope.entry);//Sends data (entry) to parent scope.
     };
 
     $scope.cancel = function () {
